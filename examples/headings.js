5cdb18d19dc9abd7abc0eb56d8b94dcac18569a3
Benjamin Eberlei <kontakt@beberlei.de>
2011-05-08 22:00:38 +0200

    Working a bit on Headings plugin, cant get the removeFormat command to work. Additionally some bug when switching between headers alot.

--- examples/headings.js
+++ examples/headings.js
@@ -9,20 +9,30 @@
         headers: [1, 2, 3]
       },
       _create: function() {
-        var buttonize, buttonset, header, widget, _i, _len, _ref;
+        var button, buttonize, buttonset, header, id, label, widget, _i, _len, _ref;
         widget = this;
         buttonset = jQuery("<span></span>");
+        id = "" + this.options.uuid + "-#paragraph";
+        label = "P";
+        buttonset.append(jQuery("<input id=\"" + id + "\" type=\"radio\" /><label for=\"" + id + "\">" + label + "</label>").button());
+        button = jQuery("#" + id, buttonset);
+        button.attr("hallo-command", "removeFormat");
+        button.bind("change", function(event) {
+          var cmd;
+          cmd = jQuery(this).attr("hallo-command");
+          alert(cmd);
+          return widget.options.editable.execute(cmd);
+        });
         buttonize = __bind(function(headerSize) {
-          var button, id, label;
           label = "H" + headerSize;
           id = "" + this.options.uuid + "-" + headerSize;
-          buttonset.append(jQuery("<input id=\"" + id + "\" type=\"checkbox\" /><label for=\"" + id + "\">" + label + "</label>").button());
+          buttonset.append(jQuery("<input id=\"" + id + "\" type=\"radio\" /><label for=\"" + id + "\">" + label + "</label>").button());
           button = jQuery("#" + id, buttonset);
           button.attr("hallo-size", "H" + headerSize);
           return button.bind("change", function(event) {
             var size;
             size = jQuery(this).attr("hallo-size");
-            return widget.options.editable.execute("heading", size);
+            return widget.options.editable.execute("formatBlock", size);
           });
         }, this);
         _ref = this.options.headers;
--- plugins/headings.coffee
+++ plugins/headings.coffee
@@ -12,15 +12,25 @@
         _create: ->
             widget = this
             buttonset = jQuery "<span></span>"
+            id = "#{@options.uuid}-#paragraph"
+            label = "P"
+            buttonset.append jQuery("<input id=\"#{id}\" type=\"radio\" /><label for=\"#{id}\">#{label}</label>").button()
+            button = jQuery "##{id}", buttonset
+            button.attr "hallo-command", "removeFormat"
+            button.bind "change", (event) ->
+                cmd = jQuery(this).attr "hallo-command"
+                alert cmd
+                widget.options.editable.execute cmd
+
             buttonize = (headerSize) =>
                 label = "H" + headerSize
                 id = "#{@options.uuid}-#{headerSize}"
-                buttonset.append jQuery("<input id=\"#{id}\" type=\"checkbox\" /><label for=\"#{id}\">#{label}</label>").button()
+                buttonset.append jQuery("<input id=\"#{id}\" type=\"radio\" /><label for=\"#{id}\">#{label}</label>").button()
                 button = jQuery "##{id}", buttonset
                 button.attr "hallo-size", "H"+headerSize
                 button.bind "change", (event) ->
                     size = jQuery(this).attr "hallo-size"
-                    widget.options.editable.execute "heading", size
+                    widget.options.editable.execute "formatBlock", size
 
             buttonize header for header in @options.headers
 
